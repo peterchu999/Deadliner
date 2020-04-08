3804cd0f2332b4a932cf1594417a85679b68d7ac
Alfon <alfonsusanto@gmail.com>
2020-04-08 15:31:51 +0700

    detail acitivty

---
+++ Deadliner/DetailActivity.swift
@@ -0,0 +1,28 @@
+//
+//  DetailActivity.swift
+//  Deadliner
+//
+//  Created by Alfon on 08/04/20.
+//  Copyright © 2020 Peter Andrew. All rights reserved.
+//
+
+import UIKit
+
+class DetailActivity: UIViewController {
+
+    @IBOutlet weak var startDate: UILabel!
+    @IBOutlet weak var endDate: UILabel!
+    @IBOutlet weak var priorityLevel: UILabel!
+    @IBOutlet weak var taskDescription: UILabel!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+
+   
+}
