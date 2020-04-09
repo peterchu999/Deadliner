36a6299e5a91161481799e07ad810fc62324fc26
Muhammad Nobel Shidqi <mnobelshidqi@gmail.com>
2020-04-09 14:56:04 +0700

    modified

--- Deadliner/Views/Table View/CustomTableViewCell.swift
+++ Deadliner/Views/Table View/CustomTableViewCell.swift
@@ -1,33 +1,33 @@
+////
+////  CustomTableViewCell.swift
+////  Deadliner
+////
+////  Created by Muhammad Nobel Shidqi on 07/04/20.
+////  Copyright © 2020 Muhammad Nobel Shidqi. All rights reserved.
+////
 //
-//  CustomTableViewCell.swift
-//  Deadliner
+//import UIKit
 //
-//  Created by Muhammad Nobel Shidqi on 07/04/20.
-//  Copyright © 2020 Muhammad Nobel Shidqi. All rights reserved.
+//class CustomTableViewCell: UITableViewCell {
 //
-
-import UIKit
-
-class CustomTableViewCell: UITableViewCell {
-
-    @IBOutlet weak var baseView: UIView!
-    @IBOutlet weak var detailView: UIView!
-    @IBOutlet weak var titleLabel: UILabel!
-    @IBOutlet weak var activitiesCountLabel: UILabel!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        baseView.layer.cornerRadius = 8
-        baseView.layer.maskedCorners = [.layerMaxXMinYCorner,.layerMaxXMaxYCorner]
-        detailView.layer.cornerRadius = 8
-        detailView.layer.maskedCorners = [.layerMaxXMinYCorner,.layerMaxXMaxYCorner]
-        
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-
-}
+//    @IBOutlet weak var baseView: UIView!
+//    @IBOutlet weak var detailView: UIView!
+//    @IBOutlet weak var titleLabel: UILabel!
+//    @IBOutlet weak var activitiesCountLabel: UILabel!
+//    
+//    override func awakeFromNib() {
+//        super.awakeFromNib()
+//        baseView.layer.cornerRadius = 8
+//        baseView.layer.maskedCorners = [.layerMaxXMinYCorner,.layerMaxXMaxYCorner]
+//        detailView.layer.cornerRadius = 8
+//        detailView.layer.maskedCorners = [.layerMaxXMinYCorner,.layerMaxXMaxYCorner]
+//        
+//    }
+//
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//
+//        // Configure the view for the selected state
+//    }
+//
+//}
